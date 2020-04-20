b02c83933c6fce94ec76cfb0676fd7bbbf38556f
peppeconti <46853326+peppeconti@users.noreply.github.com>
2020-04-20 10:40:43 +0200

    Add files

---
+++ script.js
@@ -0,0 +1,12 @@
+
+ window.addEventListener("load", function() {
+
+   
+      document.querySelector('.sottotitolo').classList.add('animate');
+      document.querySelector('.sottotitolo').style.transitionDuration = "1.3s";
+      document.querySelector('.maiuscoletto').classList.add('animate');
+      document.querySelector('.decoration_1').classList.add('animate');
+      document.querySelector('.decoration_1').style.transitionDuration = "1.3s";
+  
+
+ });
